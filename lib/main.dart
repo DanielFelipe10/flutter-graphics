fd13bb632de23d311f64a5f25dc753d77f6a688a
DanielFelipe10 <daniel.tovart@uniagustiniana.edu.co>
2023-10-24 10:37:12 -0500

    add color palette

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:math';
+/*import 'dart:math';
 
 import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
@@ -61,4 +61,4 @@ class ChartPageState extends State<ChartPage> with TickerProviderStateMixin {
       ),
     );
   }
-}
+}*/
